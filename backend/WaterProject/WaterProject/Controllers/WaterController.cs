c41048769b0b00d2af793ea10847b291f11510ff
Sarah Walker <sarahsweetpie6@gmail.com>
2025-03-17 13:09:04 -0600

    initial commit

---
+++ backend/WaterProject/WaterProject/Controllers/WaterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Data;
+
+namespace WaterProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) => _waterContext = temp;
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            return _waterContext.Projects.ToList();
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects
+                .Where(p => p.ProjectFunctionalityStatus == "Functional")
+                .ToList();
+            return something;
+        }
+    }
+}
---
+++ backend/WaterProject/WaterProject/Data/WaterDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.Data
+{
+    public class WaterDbContext : DbContext
+    {
+        public WaterDbContext(DbContextOptions<WaterDbContext> options) :base(options) 
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
