5d49eef627fadf70b7d2c1d65ff0a4d2a078af59
Sarah Walker <sarahsweetpie6@gmail.com>
2025-04-01 15:54:43 -0600

    fixed category link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/Water/GetProjectTypes'
+          'https://waterwater.azurewebsites.net/Water/GetProjectTypes'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
